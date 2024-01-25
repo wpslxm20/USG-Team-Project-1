31b8357b319e14cd52c86dddcd27073f6bad4880
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-01-25 12:47:42 +0900

    Feat: UserDetails 개발

---
+++ src/main/java/com/example/security_jwt/security/context/MemberContext.java
@@ -0,0 +1,21 @@
+package com.example.security_jwt.security.context;
+
+import com.example.security_jwt.domain.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class MemberContext extends User {
+
+    private final Member member;
+
+    public MemberContext(Member member, Collection<? extends GrantedAuthority> authorities) {
+        super(member.getEmail(), member.getPassword(), authorities);
+        this.member = member;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+}
