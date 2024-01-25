810fb2c953e4297a7cab651cec8c9c4f2c675b14
Namsoo315 <hyunsoo3315@gmail.com>
2024-01-25 20:32:54 +0900

    로그인 기능 JWT 토큰 교환 로그인 구현함 (권한설정 안함)

--- src/main/java/com/example/security_jwt/config/SecurityConfig.java
+++
@@ -1,7 +0,0 @@
-package com.example.security_jwt.config;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-
-public class SecurityConfig extends WebSecurityConfiguration{
-    
-}
