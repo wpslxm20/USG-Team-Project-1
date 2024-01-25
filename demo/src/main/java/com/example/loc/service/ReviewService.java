38f0769cb270f6c53a4132107afd0cb45adead9f
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-01-25 20:08:34 +0900

    Feat: 리뷰 서비스 레이어 개발 및 리뷰 저장 메서드 개발

---
+++ demo/src/main/java/com/example/loc/service/ReviewService.java
@@ -0,0 +1,8 @@
+package com.example.loc.service;
+
+import com.example.loc.dto.SaveReviewReqDTO;
+
+public interface ReviewService {
+
+    Long saveReview(SaveReviewReqDTO request);
+}
---
+++ demo/src/main/java/com/example/loc/service/ReviewServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.loc.service;
+
+import com.example.loc.domain.Location.Location;
+import com.example.loc.domain.Member.Member;
+import com.example.loc.domain.review.Review;
+import com.example.loc.dto.SaveReviewReqDTO;
+import com.example.loc.repository.LocationRepository;
+import com.example.loc.repository.MemberRepository;
+import com.example.loc.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ReviewServiceImpl implements ReviewService{
+
+    private final ReviewRepository reviewRepository;
+    private final MemberRepository memberRepository;
+    private final LocationRepository locationRepository;
+
+
+    @Override
+    public Long saveReview(SaveReviewReqDTO request) {
+        Member findMember = memberRepository.findById(request.getMemberId()).orElseThrow(
+                () -> new IllegalArgumentException("Member Not Exist")
+        );
+
+        Location findLocation = locationRepository.findById(request.getLocationId()).orElseThrow(
+                () -> new IllegalArgumentException("Location Not Exist")
+        );
+
+        Review review = Review
+                .builder()
+                .review(request.getReview())
+                .grade(request.getGrade())
+                .member(findMember)
+                .location(findLocation)
+                .build();
+        Review savedReview = reviewRepository.save(review);
+
+        return savedReview.getId();
+    }
+}
