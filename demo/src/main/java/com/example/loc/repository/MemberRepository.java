92f59d493a08667721f5932def19b4d02ec8a986
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-01-26 01:20:51 +0900

    Feat: 이메일을 통한 회원조회 쿼리 개발

--- demo/src/main/java/com/example/loc/repository/MemberRepository.java
+++ demo/src/main/java/com/example/loc/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.example.loc.repository;
 import com.example.loc.domain.Member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
