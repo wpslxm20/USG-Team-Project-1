d97dfb5792f3eb48be6355a5c46c67b172103075
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-01-25 20:07:48 +0900

    Feat: 리뷰 엔티티 개발

---
+++ demo/src/main/java/com/example/loc/domain/review/Review.java
@@ -0,0 +1,40 @@
+package com.example.loc.domain.review;
+
+import com.example.loc.domain.Location.Location;
+import com.example.loc.domain.Member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review {
+
+    @Id @GeneratedValue
+    @Column(name = "review_id")
+    private Long id;
+    private String review;
+    private Double grade;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "reg_id")
+    private Location location;
+
+    // 사진 필요여부 이후 결정
+
+    @Builder
+    public Review(String review, Double grade, Member member, Location location) {
+        this.review = review;
+        this.grade = grade;
+        this.member = member;
+        this.location = location;
+    }
+}
