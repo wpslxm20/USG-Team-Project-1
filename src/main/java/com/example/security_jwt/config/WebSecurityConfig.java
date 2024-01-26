16081dd6235ef767e2ec555f3c064aa2387616fd
Namsoo315 <hyunsoo3315@gmail.com>
2024-01-26 19:46:53 +0900

    config 전체 허용으로 다시 바꿈.

--- src/main/java/com/example/security_jwt/config/WebSecurityConfig.java
+++ src/main/java/com/example/security_jwt/config/WebSecurityConfig.java
@@ -13,8 +13,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -27,7 +25,30 @@ public class WebSecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
     @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.formLogin().disable();
+        http.httpBasic().disable();
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .cors() // cors 커스텀 설정
+                .and()
+
+                .sessionManagement()//세션 사용 x
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+
+    /*@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -36,6 +57,7 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))//세션 사용 x
                 .authorizeHttpRequests(auth ->auth
+                        .requestMatchers("/api/customer/mypage/modify").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/login", "api/signup", "api/resissue").permitAll()
                         .requestMatchers("/api/login","/api/resissue","api/signup").permitAll()
                         .anyRequest().authenticated())
@@ -44,5 +66,5 @@ public class WebSecurityConfig {
         http
                 .userDetailsService(customUserDetailsService);
         return http.build();
-    }
+    }*/
 }
