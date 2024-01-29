16fc2537177caf0c42965c3bda9472474db81f50
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-01-29 15:39:55 +0900

    Feat: 스웨거 설정 클래스 개발

---
+++ demo/src/main/java/com/example/loc/global/swagger/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.example.loc.global.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "업소 / 리뷰 API 명세서",
+        description = "USG 프로젝트의 업소 / 리뷰 API 명세서 입니다.",
+        version = "v1")
+)
+public class SwaggerConfig {
+
+    /**
+     * http://localhost:8080/swagger-ui/index.html
+     */
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement));
+    }
+}
